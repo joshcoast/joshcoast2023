7f4784412ca57e2388961b5f51a7e041f6dca12a
Josh Coast <josh@joshcoast.com>
2023-02-22 21:14:50 -0700

    empty out repo

--- app/public/wp-content/themes/joshcoast/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '7f9258da9fef5ef56c27');
--- app/public/wp-content/uploads/siteground-optimizer-assets/siteground-optimizer-combined-js-3ba2afadf516e5351f895319f8994792.js
+++
@@ -1,3 +0,0 @@
-document.body.className = document.body.className.replace('no-js','js');;
-try{document.getElementById('user_login').focus();}catch(e){}
-		if(typeof wpOnload==='function')wpOnload();
